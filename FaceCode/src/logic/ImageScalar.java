fa425192dfa548b6aaa8864ff17a310286da3a97
dinamix <tristano.tenaglia@gmail.com>
2014-11-15 11:42:01 -0500

    imagescalar and test

---
+++ FaceCode/src/logic/ImageScalar.java
@@ -0,0 +1,17 @@
+package logic;
+
+import java.awt.image.BufferedImage;
+import org.imgscalr.Scalr; //imgscalr imported library but not native. built using base java libraries
+
+/*
+ * Scalr Library:
+ * http://www.thebuzzmedia.com/downloads/software/imgscalr/javadoc/org/imgscalr/Scalr.html
+ */
+
+public class ImageScalar {
+	public static BufferedImage reSize(BufferedImage image,int width,int height) {
+		//return BufferedImage Object with specified height and width
+		//given from org.imgscalr.Scalr library
+		return Scalr.resize(image,width,height);
+	}
+}
