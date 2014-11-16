453a4283a59955d4f7e18823dbd67d88cadf6055
dandoman <davide.cannucci@gmail.com>
2014-11-16 04:02:00 -0500

    Fixing merge problem

--- FaceCode/src/data/Face.java
+++ FaceCode/src/data/Face.java
@@ -8,17 +8,11 @@ public class Face implements Serializable{
 	private static final long serialVersionUID = 3594111941132367353L;
 	private String faceName;
 	private BufferedImage originalFace;
-	private int originalImageHeight;
-	private int originalImageWidth;
-	private int [] originalFaceVector;
-	private int [][] eigenFace;
 	private int [] faceDifference;
 	
 	public Face(String faceName, BufferedImage originalFace){
 		this.faceName = faceName;
 		this.originalFace = originalFace;
-		originalImageHeight = originalFace.getHeight();
-		originalImageWidth = originalFace.getWidth();
 	}
 	
 	public void setFaceDifference(int [] faceDifference){
@@ -34,7 +28,6 @@ public class Face implements Serializable{
 	}
 	
 	public int [] getOriginalFaceVector(){
-		int [][] faceVector = new int[originalImageHeight][originalImageWidth];
 		return null;
 	}
 	
